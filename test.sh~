864d56f4eadca7d370153203624d2f1974fc4c5b
Yan Paing Oo <yanpaingoo008ypo@gmail.com>
2021-07-11 15:35:42 +0630

    v1.0.1
    Delete unnecessary Files

--- test.sh~
+++
@@ -1,3 +0,0 @@
-read -p "Mail : " email
-read -p "Domain:" domain
-sudo certbot certonly --standalone --preferred-challenges http --agree-tos --email ${email} -d ${domain}
--- v2ray_setup.sh
+++ v2ray_setup.sh
@@ -2,74 +2,9 @@
 #Progress Bar 
 read -p "Enter your mail:" email
 read -p "Enter your doman:" domain
-progress_bar()
-{
-  local DURATION=$1
-  local INT=0.25      # refresh interval
-
-  local TIME=0
-  local CURLEN=0
-  local SECS=0
-  local FRACTION=0
-
-  local FB=2588       # full block
-
-  trap "echo -e $(tput cnorm); trap - SIGINT; return" SIGINT
-
-  echo -ne "$(tput civis)\r$(tput el)│"                # clean line
-
-  local START=$( date +%s%N )
-
-  while [ $SECS -lt $DURATION ]; do
-    local COLS=$( tput cols )
-
-    # main bar
-    local L=$( bc -l <<< "( ( $COLS - 5 ) * $TIME  ) / ($DURATION-$INT)" | awk '{ printf "%f", $0 }' )
-    local N=$( bc -l <<< $L                                              | awk '{ printf "%d", $0 }' )
-
-    [ $FRACTION -ne 0 ] && echo -ne "$( tput cub 1 )"  # erase partial block
-
-    if [ $N -gt $CURLEN ]; then
-      for i in $( seq 1 $(( N - CURLEN )) ); do
-        echo -ne \\u$FB
-      done
-      CURLEN=$N
-    fi
-
-    # partial block adjustment
-    FRACTION=$( bc -l <<< "( $L - $N ) * 8" | awk '{ printf "%.0f", $0 }' )
-
-    if [ $FRACTION -ne 0 ]; then 
-      local PB=$( printf %x $(( 0x258F - FRACTION + 1 )) )
-      echo -ne \\u$PB
-    fi
-
-    # percentage progress
-    local PROGRESS=$( bc -l <<< "( 100 * $TIME ) / ($DURATION-$INT)" | awk '{ printf "%.0f", $0 }' )
-    echo -ne "$( tput sc )"                            # save pos
-    echo -ne "\r$( tput cuf $(( COLS - 6 )) )"         # move cur
-    echo -ne "│ $PROGRESS%"
-    echo -ne "$( tput rc )"                            # restore pos
-
-    TIME=$( bc -l <<< "$TIME + $INT" | awk '{ printf "%f", $0 }' )
-    SECS=$( bc -l <<<  $TIME         | awk '{ printf "%d", $0 }' )
-
-    # take into account loop execution time
-    local END=$( date +%s%N )
-    local DELTA=$( bc -l <<< "$INT - ( $END - $START )/1000000000" \
-                   | awk '{ if ( $0 > 0 ) printf "%f", $0; else print "0" }' )
-    sleep $DELTA
-    START=$( date +%s%N )
-  done
-
-  echo $(tput cnorm)
-  trap - SIGINT
-}
-
 #Server Configure 
 
 echo "Updating apt and changing timezone"
-progress_bar 10
 
 apt-get update -y
 echo "Update Finished"
@@ -79,17 +14,14 @@ timedatectl set-timezone Asia/Yangon
 echo "Timezone Changed"
 
 echo "Installing Requirements"
-progress_bar 10
 #Requirement Installation 
 apt install software-properties-common -y 
 add-apt-repository ppa:certbot/certbot -y 
 apt install curl -y 
 apt-get install certbot -y 
 echo "Start Install v2ray"
-progress_bar 10
 #Install v2ray UI script 
 bash <(curl -Ls https://blog.sprov.xyz/v2-ui.sh)
-progress_bar 10
 #Get SSL Certificate 
 if [ -v "$email" ]; then 
 	echo "Generate Cerbot"
